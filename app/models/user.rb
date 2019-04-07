12ce584228209a87bf3985730c49b6651da5c9a5
CJ Avilla <cjavilla@gmail.com>
2019-04-07 16:57:08 -0700

    Initial2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  email           :string           not null
+#  password_digest :string
+#  session_token   :string           not null
+#  deleted_at      :datetime
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_deleted_at     (deleted_at)
+#  index_users_on_email          (email)
+#  index_users_on_session_token  (session_token)
+#
+
 class User < ApplicationRecord
 end
